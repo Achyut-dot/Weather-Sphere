5b1cc3a89e53258a6ed664338880b2035fea1ed1
Achyut Raghuvanshi <achyut24122002@gmail.com>
2025-03-23 17:32:37 +0530

    Environment Created For the Application with the API Key

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    openWeatherApiKey: 'e5c07d2c9b970b0c7ed7faf6173f6a20',
+    openWeatherBaseUrl: 'https://api.openweathermap.org/data/2.5',
+    openWeatherIconUrl: 'https://openweathermap.org/img/wn',
+    geoLocationApiUrl: 'https://api.openweathermap.org/geo/1.0'
+};
